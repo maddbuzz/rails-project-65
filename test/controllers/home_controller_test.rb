91b05166adc5587e8ea6edb7e83376c4fb87e6b4
Andrey Anisimov <maddbuzz@gmail.com>
2023-03-02 15:47:08 +0800

    work on step 1

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,7 +4,8 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
-    get home_index_url
+    # get home_index_url
+    get root_url
     assert_response :success
   end
 end
