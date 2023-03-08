7a9592c8c91e8b5b761a48d232577ba2b7271355
Andrey Anisimov <maddbuzz@gmail.com>
2023-03-08 21:01:22 +0800

    &

--- db/migrate/20230307091437_add_admin.rb
+++ db/migrate/20230307091437_add_admin.rb
@@ -2,6 +2,6 @@
 
 class AddAdmin < ActiveRecord::Migration[7.0]
   def change
-    User.find_by(email: 'maddbuzz@gmail.com').update(admin: true)
+    User.find_by(email: 'maddbuzz@gmail.com')&.update(admin: true)
   end
 end
